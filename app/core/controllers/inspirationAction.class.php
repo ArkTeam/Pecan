6350ba0983052b985e2e76fa35d9489fb52e6bfd
Ark <onlyarter@gmail.com>
2015-05-29 16:01:06 +0800

    添加  inspirations模版  添加显示功能

--- app/core/controllers/inspirationAction.class.php
+++ app/core/controllers/inspirationAction.class.php
@@ -1,12 +1,16 @@
  <?php
 	class InspirationAction extends Controller {
 		protected $inpiration;
-		
-		function listInspirations() {
+		function listInspirations($s, $o) {
+			if(!isset($s)||!isset($o)){
+				$s=0;
+				$o=8;
+			}
 			$this->inpiration = new ArkInspiration ();
-			$inpirations = $this->inpiration->getInspirations ( 0, 5 );
+			$inspirations = $this->inpiration->getInspirations ( $s, $o );
+			$this->tpl_x->assign ( 'inspirations', $inspirations );
+			$this->display ( 'inspirations.html' );
 		}
-		
 		function addAnInspiration($category_id, $inspiration_content) {
 			$this->inpiration = new ArkInspiration ();
 			if ($this->inpiration->createInspiration ( $category_id, $inspiration_content ) == 1) {
